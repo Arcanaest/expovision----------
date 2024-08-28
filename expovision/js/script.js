3c528e3e5a3327d4fe4c7be4fc277978f3fb2fa6
Arcanaest <f9d3@mail.ru>
2024-08-28 13:24:35 +0300

    add Expovision Maket

---
+++ expovision/js/script.js
@@ -0,0 +1,32 @@
+const btn = document.getElementById('application');
+const form = document.getElementById('form__port');
+
+btn.addEventListener('click', () => {
+  form.style.display = 'flex';
+});
+
+form.addEventListener('click', (e) => {
+  if (e.target.classList.contains('form__port')) {
+    form.style.display = 'none';
+  }
+});
+
+function Marquee(selector, speed) {
+  const parentSelector = document.querySelector(selector);
+  const clone = parentSelector.innerHTML;
+  const firstElement = parentSelector.children[0];
+  let i = 0;
+  console.log(firstElement);
+  parentSelector.insertAdjacentHTML('beforeend', clone);
+  parentSelector.insertAdjacentHTML('beforeend', clone);
+
+  setInterval(function () {
+    firstElement.style.marginLeft = "-" + i + "px";
+    if (i > firstElement.clientWidth) {
+      i = 0;
+    }
+    i = i + speed;
+  }, 0);
+}
+window.addEventListener('load', Marquee('.marquee', 0.2));
+
